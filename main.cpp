5c0967ac6948e717324e0a1266076abdf2a5f61a
ye Loathsome <doudqwer@gmail.com>
2026-02-02 16:25:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 ConfigManager cfgMgr;
 
+bool parseMac(const char* str, uint8_t* mac) {
+  return sscanf(
+    str,
+    "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx",
+    &mac[0], &mac[1], &mac[2],
+    &mac[3], &mac[4], &mac[5]
+  ) == 6;
+}
+
 void setup() {
   Serial.begin(115200);
   cfgMgr.begin();
@@ -19,11 +28,18 @@ void loop() {
   if (deserializeJson(doc, line)) return;
 
   Config cfg = {};
+
   strlcpy(cfg.wifiSSID, doc["wifiSSID"] | "", sizeof(cfg.wifiSSID));
   strlcpy(cfg.wifiPassword, doc["wifiPassword"] | "", sizeof(cfg.wifiPassword));
   strlcpy(cfg.botToken, doc["botToken"] | "", sizeof(cfg.botToken));
   strlcpy(cfg.pcTargetIP, doc["pcTargetIP"] | "192.168.1.150", sizeof(cfg.pcTargetIP));
   strlcpy(cfg.psTargetIP, doc["psTargetIP"] | "192.168.1.100", sizeof(cfg.psTargetIP));
+
+  const char* macStr = doc["macAddress"] | "";
+  if (!parseMac(macStr, cfg.macAddress)) {
+    Serial.println("INVALID_MAC");
+    return;
+  }
 
   cfgMgr.save(cfg);
 
